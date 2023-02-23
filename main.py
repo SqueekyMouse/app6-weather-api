c3e95aa143e86504c4e01ea179cbc82f92b018d8
sreejith <squeeky.mouse@gmail.com>
2023-02-23 20:03:00 +0530

    show stations table on homepage Sec31

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask,render_template
 import pandas as pd
-#commit: return temp for a station Sec30
+#commit: show stations table on homepage Sec31
 
 app=Flask(__name__)# to enable????
+
+stations=pd.read_csv('data_small/stations.txt',skiprows=17)
+stations=stations[['STAID','STANAME                                 ']]
 
 # @means this is decorator and it decorates the fn below!!!
 @app.route('/')
 def home():
-    return(render_template('home.html')) 
+    return(render_template('home.html',data=stations.to_html())) ## kwargs!!!
 
 @app.route('/api/v1/<station>/<date>')#get args from url!!
 def about(station,date):
-    st_fill=str(station).zfill(6) #fillnum with leading zeros!!! 
+    st_fill=str(station).zfill(6) #fillnum with leading zeros!!!
     df=pd.read_csv(f'data_small/TG_STAID{st_fill}.txt',skiprows=20,parse_dates=['    DATE'])
     temparature=df.loc[df['    DATE']==date]['   TG'].squeeze()/10
     return({'station':station,
