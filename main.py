3318ebdaaf677c049b72caafc2738900a4278555
sreejith <squeeky.mouse@gmail.com>
2023-02-23 20:58:57 +0530

    add url, endpoint for yearly and all data Sec31

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 import pandas as pd
-#commit: show stations table on homepage Sec31
+#commit: add url, endpoint for yearly and all data Sec31
 
 app=Flask(__name__)# to enable????
 
@@ -20,6 +20,25 @@ def about(station,date):
     return({'station':station,
             'date':date,
             'temparature':temparature})
+
+@app.route('/api/v1/<station>')
+def all_data(station):
+    st_file=str(station).zfill(6)
+    df=pd.read_csv(f'data_small/TG_STAID{st_file}.txt',skiprows=20,parse_dates=['    DATE'])
+    # result=df.to_dict() #returns dict of columns
+    result=df.to_dict(orient='records')# return list of dict of rows
+    return(result)
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station,year):
+    st_file=str(station).zfill(6)
+    # df=pd.read_csv(f'data_small/TG_STAID{st_file}.txt',skiprows=20,parse_dates=['    DATE'])
+    # result=df[df['    DATE'].str.startswith(str(year))] #will err out as date has been parsed to date type!!!
+    df=pd.read_csv(f'data_small/TG_STAID{st_file}.txt',skiprows=20)
+    df['    DATE']=df['    DATE'].astype(str) # convert dates to srt!!!
+    result=df[df['    DATE'].str.startswith(str(year))]
+    result=result.to_dict(orient='records')
+    return(result)
 
 if __name__=='__main__':
     # app.run(port=5001,debug=True) # specify alt port if running multiple flask apps!!!
